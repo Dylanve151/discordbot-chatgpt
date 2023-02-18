8718c1c1c04956ebe565d763a48dcf48d8fa9c00
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2023-02-18 16:43:01 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 
 import discord
 import openai
@@ -13,24 +14,28 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 OPENAI_TOKEN = os.getenv('OPENAI_TOKEN')
 openai.api_key = OPENAI_TOKEN
 
+async def respsoneandsent(chatmsg, question):
+	print("Q:\"", question, "\"")
+	response = openai.Completion.create(
+		model="text-davinci-003", 
+		max_tokens=800,
+		temperature=0.5,
+		presence_penalty=0,
+		frequency_penalty=0,
+		best_of=1,
+		prompt=question
+	)
+	embed = discord.Embed(title=question, description=response["choices"][0]["text"])
+	await chatmsg.reply(embed=embed)
+	print("A:\"", response.choices[0].text, "\"")
+
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+	print(f'We have logged in as {bot.user}')
 
 @bot.command()
 async def chatgpt(ctx, *, args):
-    print("Q:\"", args, "\"")
-    response = openai.Completion.create(
-        model="text-davinci-003", 
-        max_tokens=800,
-        temperature=0.5,
-        presence_penalty=0,
-        frequency_penalty=0,
-        best_of=1,
-        prompt=args
-    )
-    embed = discord.Embed(title=args, description=response["choices"][0]["text"])
-    await ctx.reply(embed=embed)
-    print("A:\"", response.choices[0].text, "\"")
+	cmd = respsoneandsent(ctx, args)
+	task = asyncio.create_task(cmd)
 
 bot.run(BOT_TOKEN)
